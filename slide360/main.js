31f664ea3b86a1556b5fe55f833258d1f1fff36f
suhom118 <suhom118@gmail.com>
2022-11-11 13:03:29 +0900

    utyikty

--- slide360/main.js
+++ slide360/main.js
@@ -8,8 +8,7 @@ gsap
     rotateY: (i) => i * -36,
     transformOrigin: "50% 50% 500px",
     z: -500,
-    backgroundImage: (i) =>
-      "url(https://picsum.photos/id/" + (i + 32) + "/600/400/)",
+    backgroundImage: (i) => "url(img" + (i + 32) + ".jpg)",
     backgroundPosition: (i) => getBgPos(i),
     backfaceVisibility: "hidden",
   })
